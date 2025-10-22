6794c9665df9d7cb56329ceb379a124288ff5edf
Annika Törnberg <amk1012601@student.hamk.fi>
2025-10-22 10:33:39 +0300

    Koodin lisäys
    Lisätty koodi

---
+++ Koodi/Ika-ohjelma-harjoitus.java
@@ -0,0 +1,31 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int ika = 20;
+
+        if (ika > 0 && ika < 18) {
+            System.out.println("Olet alaikäinen");
+
+            if (ika >= 15) {
+                System.out.println("Saat ajaa mopoa");
+            }
+
+
+            if (ika == 17) {
+                System.out.println("Saat ajaa kevaria");
+
+                //Tama koodi testaa onko ika 17
+
+                //Testi - kommentti
+
+
+            }
+
+        } else if (ika >= 65) {
+            System.out.println("Olet eläkeläinen");
+
+        } else {
+            System.out.println("Olet aikuinen");
+        }
+
+    }
+}
